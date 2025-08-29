e6eb67fd30a9fb30181808d7f732a35e8f30c626
NIKHILA REDDY <iamnikhilareddy@gmail.com>
2025-08-29 22:38:34 +0530

    feat: add Listing model with basic info fields (title, description, price, location, country)

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ let app=express();
 const path = require("path");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/wonderlust')
+const Listing=require("./models/listing.js");
+mongoose.connect('mongodb://127.0.0.1:27017/wanderlust')
 .then(() => console.log('Connected!'))
 .catch((err)=>{
     console.log(err);
@@ -14,7 +15,17 @@ mongoose.connect('mongodb://127.0.0.1:27017/wonderlust')
 app.get("/",(req,res)=>{
     res.send("hi i am root");
 })
-
+app.get("/allListing",async (req,res)=>{
+    let listing=new Listing({
+        title:"Agni Studio with Pool across Talpona River",
+        description:"2 guests1 bedroom1 bed1 bathroom",
+        price:1200,
+        location:"Entire rental unit in Canacona",
+        country:"India"
+    })
+    await listing.save();
+    res.send("succcesful");
+})
 app.listen(8080,()=>{
     console.log("server is listening on port 8080");
 });
---
+++ models/listing.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const Schema=mongoose.Schema;
+const listingSchema=new Schema({
+    title:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String
+    },
+    image:{
+        type:String,
+        default:"https://www.shutterstock.com/image-illustration/random-pictures-cute-funny-2286554497?dd_referrer=https%3A%2F%2Fwww.google.com%2F",
+        set:(v)=> v==="" ? "https://www.shutterstock.com/image-illustration/random-pictures-cute-funny-2286554497?dd_referrer=https%3A%2F%2Fwww.google.com%2F" : v,
+    },
+    price:{
+        type:String
+    },
+    location:{
+        type:String
+    },
+    country:{
+        type:String
+    }
+    
+});
+
+const Listing=mongoose.model("Listing",listingSchema);
+
+module.exports=Listing;
