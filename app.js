9ed372b61719bfc5b32e07840f85da95a8f7ad84
NIKHILA REDDY <iamnikhilareddy@gmail.com>
2025-08-30 11:29:24 +0530

    feat: fetch and display all listing titles in index route

--- app.js
+++ app.js
@@ -1,31 +1,34 @@
+// require express
 const express = require("express");
 let app=express();
-const path = require("path");
+
+// require ejs
 const ejs = require("ejs");
+const path = require("path");
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+
+// require mongoose and connecting with mongoose
 const mongoose = require('mongoose');
-const Listing=require("./models/listing.js");
 mongoose.connect('mongodb://127.0.0.1:27017/wanderlust')
 .then(() => console.log('Connected!'))
 .catch((err)=>{
     console.log(err);
 });
+const Listing=require("./models/listing.js");
 
+// index route
+app.get("/listings",async (req,res)=>{
+    let allListings=await Listing.find({});
+    res.render("./listings/index.ejs",{allListings});
+})
 
-
+// root route
 app.get("/",(req,res)=>{
     res.send("hi i am root");
 })
-app.get("/allListing",async (req,res)=>{
-    let listing=new Listing({
-        title:"Agni Studio with Pool across Talpona River",
-        description:"2 guests1 bedroom1 bed1 bathroom",
-        price:1200,
-        location:"Entire rental unit in Canacona",
-        country:"India"
-    })
-    await listing.save();
-    res.send("succcesful");
-})
+
+//checking server listening or not
 app.listen(8080,()=>{
     console.log("server is listening on port 8080");
 });
