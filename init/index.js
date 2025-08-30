c483cfd448960ad273909130c6fd3ee8124e4e77
NIKHILA REDDY <iamnikhilareddy@gmail.com>
2025-08-30 10:46:56 +0530

    feat: add initial seed data to database

---
+++ init/index.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const initData=require("./data.js");
+const Listing=require("../models/listing.js");
+mongoose.connect('mongodb://127.0.0.1:27017/wanderlust')
+.then(() => console.log('Connected!'))
+.catch((err)=>{
+    console.log(err);
+});
+
+const initDB=async()=>{
+    await Listing.deleteMany({});
+    await Listing.insertMany(initData.data);
+    console.log("initialized data");
+}
+initDB();
--- models/listing.js
+++ models/listing.js
@@ -9,9 +9,16 @@ const listingSchema=new Schema({
         type:String
     },
     image:{
-        type:String,
-        default:"https://www.shutterstock.com/image-illustration/random-pictures-cute-funny-2286554497?dd_referrer=https%3A%2F%2Fwww.google.com%2F",
-        set:(v)=> v==="" ? "https://www.shutterstock.com/image-illustration/random-pictures-cute-funny-2286554497?dd_referrer=https%3A%2F%2Fwww.google.com%2F" : v,
+        filename: {
+            type: String,
+            default: "listingimage"
+        },
+        
+            url:{
+                type:String,
+            default:"https://www.shutterstock.com/image-illustration/random-pictures-cute-funny-2286554497?dd_referrer=https%3A%2F%2Fwww.google.com%2F",
+            set:(v)=> v==="" ? "https://www.shutterstock.com/image-illustration/random-pictures-cute-funny-2286554497?dd_referrer=https%3A%2F%2Fwww.google.com%2F" : v,
+            }
     },
     price:{
         type:String
