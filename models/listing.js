e8a7cacacfec9b057a6526a09a405b9fa8d8d4e8
NIKHILA REDDY <iamnikhilareddy@gmail.com>
2025-09-03 21:22:18 +0530

    refactor(model): update default image link in listing schema

--- models/listing.js
+++ models/listing.js
@@ -16,7 +16,7 @@ const listingSchema=new Schema({
         
             url:{
                 type:String,
-            default:"https://www.shutterstock.com/image-illustration/random-pictures-cute-funny-2286554497?dd_referrer=https%3A%2F%2Fwww.google.com%2F",
+            default:"https://images.unsplash.com/photo-1507525428034-b723cf961d3e",
             set:(v)=> v==="" ? "https://www.shutterstock.com/image-illustration/random-pictures-cute-funny-2286554497?dd_referrer=https%3A%2F%2Fwww.google.com%2F" : v,
             }
     },
