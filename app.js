884e13334c0e441d14eb29f9bfd194c42bafc3d2
NIKHILA REDDY <iamnikhilareddy@gmail.com>
2025-09-11 21:36:40 +0530

    refactor(routes): modularize /listings with Express Router

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 const express = require("express");
 let app=express();
 
+// Import listings route file
+const listings=require("./routes/listing.js");
+
 // Import EJS and ejs-mate (for layouts/partials support)
 const ejs = require("ejs");
 const engine = require('ejs-mate');
@@ -12,18 +15,6 @@ const wrapAsync = require("./utils/wrapAsync.js");
 
 // Import custom error class for handling HTTP errors
 const ExpressError = require("./utils/ExpressError.js");
-
-// Validates listing data and throws an error if invalid.  
-const {listingSchema}=require("./schema.js");
-const validateListing=(req,res,next)=>{
-    let {error}=listingSchema.validate(req.body);
-    if(error){
-        let errMsg=error.details.map((el)=>el.message).join(",");
-        throw new ExpressError(400,errMsg);
-    }else{
-        next();
-    }
-}
 
 // Validates reviews data and throws an error if invalid.
 const {reviewSchema}=require("./schema.js");
@@ -66,51 +57,7 @@ const Listings=require("./models/listing.js");
 // Import Reviews model
 const Reviews=require("./models/review.js");
 
-// index route
-app.get("/listings",wrapAsync(async (req,res)=>{
-    let allListings=await Listings.find({});
-    res.render("./listings/index.ejs",{allListings});
-}))
-
-// New route
-app.get("/listings/new",(req,res)=>{
-    res.render("./listings/new.ejs")
-})
-
-// Create route
-app.post("/listings",validateListing,wrapAsync( async(req,res)=>{
-    let listing=new Listings(req.body.listing);
-    await listing.save();
-    res.redirect("/listings");
-}))
-
-// show route
-app.get("/listings/:id",wrapAsync(async (req,res)=>{
-    let {id}=req.params;
-    let listing= await Listings.findById(id).populate("reviews");
-    res.render("./listings/show.ejs",{listing});
-}))
-
-// Edit route
-app.get("/listings/:id/edit",wrapAsync(async (req,res)=>{
-    let {id}=req.params;
-    let listing= await Listings.findById(id);
-    res.render("listings/edit.ejs",{listing});
-}))
-
-// Update Route
-app.put("/listings/:id",validateListing,wrapAsync(async (req,res)=>{
-    let {id}=req.params;
-    await Listings.findByIdAndUpdate(id,{...req.body.listing});
-    res.redirect(`/listings/${id}`);
-}))
-
-// Delete Route
-app.delete("/listings/:id",wrapAsync(async (req,res)=>{
-    let {id}=req.params;
-    let deletedList=await Listings.findByIdAndDelete(id);
-    res.redirect("/listings");
-}))
+app.use("/listings",listings);
 
 // Review route
 app.post("/listings/:id/reviews",validateReview,wrapAsync(async(req,res)=>{
---
+++ routes/listing.js
@@ -0,0 +1,75 @@
+// Import Express
+const express = require("express");
+
+// Import the Express Router object (a mini version of the app just for routes)
+const router=express.Router();
+
+// Import utility to handle async errors
+const wrapAsync = require("../utils/wrapAsync.js");
+
+// Import custom error class for handling HTTP errors
+const ExpressError = require("../utils/ExpressError.js");
+
+// Validates listing data and throws an error if invalid.  
+const {listingSchema}=require("../schema.js");
+const validateListing=(req,res,next)=>{
+    let {error}=listingSchema.validate(req.body);
+    if(error){
+        let errMsg=error.details.map((el)=>el.message).join(",");
+        throw new ExpressError(400,errMsg);
+    }else{
+        next();
+    }
+}
+
+// Import Listing model
+const Listings=require("../models/listing.js");
+
+// index route
+router.get("/",wrapAsync(async (req,res)=>{
+    let allListings=await Listings.find({});
+    res.render("./listings/index.ejs",{allListings});
+}))
+
+// New route
+router.get("/new",(req,res)=>{
+    res.render("./listings/new.ejs")
+})
+
+// Create route
+router.post("/",validateListing,wrapAsync( async(req,res)=>{
+    let listing=new Listings(req.body.listing);
+    await listing.save();
+    res.redirect("/listings");
+}))
+
+// show route
+router.get("/:id",wrapAsync(async (req,res)=>{
+    let {id}=req.params;
+    let listing= await Listings.findById(id).populate("reviews");
+    res.render("./listings/show.ejs",{listing});
+}))
+
+// Edit route
+router.get("/:id/edit",wrapAsync(async (req,res)=>{
+    let {id}=req.params;
+    let listing= await Listings.findById(id);
+    res.render("listings/edit.ejs",{listing});
+}))
+
+// Update Route
+router.put("/:id",validateListing,wrapAsync(async (req,res)=>{
+    let {id}=req.params;
+    await Listings.findByIdAndUpdate(id,{...req.body.listing});
+    res.redirect(`/listings/${id}`);
+}))
+
+// Delete Route
+router.delete("/:id",wrapAsync(async (req,res)=>{
+    let {id}=req.params;
+    let deletedList=await Listings.findByIdAndDelete(id);
+    res.redirect("/listings");
+}))
+
+// Export router so it can be used in app.js
+module.exports = router;
